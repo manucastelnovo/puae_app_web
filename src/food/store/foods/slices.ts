5bd8e48f0ebcaac8031cb71fa55490699e743293
manucastelnovo <marnutskate@gmail.com>
2024-07-18 15:39:10 -0400

    add food slice

--- src/food/store/foods/slices.ts
+++ src/food/store/foods/slices.ts
@@ -1,13 +1,13 @@
-import { createSlice, type  PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 
 export type FoodId = string;
 
 export interface Food {
-    foodName:string;
-    profit:number;
-    productionCost:number;
-    description:string;
-    foodType:string;
+    foodName: string;
+    profit: number;
+    productionCost: number;
+    description: string;
+    foodType: string;
 }
 
 export interface FoodWithId extends Food {
@@ -17,17 +17,17 @@ export interface FoodWithId extends Food {
 const DEFAULT_STATE: FoodWithId[] = [
     {
         id: "1",
-        foodName:"burger",
-        profit:2000,
-        productionCost:2000,
-        description:"good food",
-        foodType:"meal"
+        foodName: "burger",
+        profit: 2000,
+        productionCost: 2000,
+        description: "good food",
+        foodType: "meal"
     }
 ]
 
-const initialState: FoodWithId[] = (()=>{
-    const persistedState= localStorage.getItem("__redux__state__food")
-    if(persistedState){
+const initialState: FoodWithId[] = (() => {
+    const persistedState = localStorage.getItem("__redux__state__food")
+    if (persistedState) {
         return JSON.parse(persistedState).users
     }
     return DEFAULT_STATE;
@@ -36,7 +36,7 @@ export const foodsSlice = createSlice({
     name: 'foods',
     initialState: initialState,
     reducers: {
-        deleteFoodById: (state, action:PayloadAction<FoodId>) => {
+        deleteFoodById: (state, action: PayloadAction<FoodId>) => {
             const id = action.payload;
             return state.filter((food) => food.id != id)
         }
