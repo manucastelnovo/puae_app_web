6603c92aabdf2efa49aeac2db361c43168c1d0d1
manucastelnovo <marnutskate@gmail.com>
2024-07-19 09:47:09 -0400

    remove space

--- src/services/store/index.ts
+++ src/services/store/index.ts
@@ -2,9 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import foodsReducer from '../../food/data/store/slices'
 
 
-
-
-
 export const store = configureStore({
   reducer: {
     foods: foodsReducer,
